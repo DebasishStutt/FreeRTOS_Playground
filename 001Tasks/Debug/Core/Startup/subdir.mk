e31c54f06f745d725cbcb2e3de696afba727f3da
Debasish Mukherjee <debasishformasters@gmail.com>
2024-01-06 09:35:12 +0100

    Reconfigure to build

--- 001Tasks/Debug/Core/Startup/subdir.mk
+++ 001Tasks/Debug/Core/Startup/subdir.mk
@@ -16,7 +16,7 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"D:/All_Learnings/Embedded-ALL/FreeRTOS_Restart_21_12_23/ide_wrkspc/RTOS_work/001Tasks/ThirdParty/SEGGER/SEGGER" -I"D:/All_Learnings/Embedded-ALL/FreeRTOS_Restart_21_12_23/ide_wrkspc/RTOS_work/001Tasks/ThirdParty/SEGGER/Config" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"D:/All_Learnings/Embedded-ALL/FreeRTOS_Restart_21_12_23/ide_wrkspc/RTOS_work/FreeRTOS_Playground/001Tasks/ThirdParty/SEGGER/SEGGER" -I"D:/All_Learnings/Embedded-ALL/FreeRTOS_Restart_21_12_23/ide_wrkspc/RTOS_work/FreeRTOS_Playground/001Tasks/ThirdParty/SEGGER/Config" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
